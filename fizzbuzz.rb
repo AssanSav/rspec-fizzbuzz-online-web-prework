3fc40cfa86835c6218fcfcc746fe780086429193
assane savadogo <assane.savadogo81@gmail.com>
2019-05-30 18:24:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,10 @@
 def fizzbuzz(num)
-  elsif num % 3 == 0 and num % 5 == 0 
+if num % 3 == 0 and num % 5 == 0 
   return "FizzBuzz"
- if num % 3 == 0
+elsif num % 3 == 0
   return "Fizz"
 elsif num % 5 == 0 
   return "Buzz"
-
 else 
   return nil
 end
